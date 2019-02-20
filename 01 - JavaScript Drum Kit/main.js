f2efa801ee6c2c3586e6e325ed2dcf9687829fb8
siufai6 <siufai6@gmail.com>
2019-02-20 15:33:16 +0800

    Add files via upload
    01 drum kit

---
+++ 01 - JavaScript Drum Kit/main.js
@@ -0,0 +1,14 @@
+addEvent(document, "keypress", function (e) {
+    e = e || window.event;
+    // use e.keyCode
+});
+
+function addEvent(element, eventName, callback) {
+    if (element.addEventListener) {
+        element.addEventListener(eventName, callback, false);
+    } else if (element.attachEvent) {
+        element.attachEvent("on" + eventName, callback);
+    } else {
+        element["on" + eventName] = callback;
+    }
+}
